8a0538bb9ee6d8bce953e99d24013c9b901fb9ad
jersonarnual <jerson8moreno@gmail.com>
2021-04-15 10:13:29 -0500

    Agregar archivos de proyecto.

---
+++ Domain/Configuracion/BaseEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Alpha.Domain
+{
+    public class BaseEntity
+    {
+        public DateTime CreateTime { get; set; }
+        public DateTime UpdateTime { get; set; }
+        public string CreateBy { get; set; }
+        public string UpdateBy { get; set; }
+    }
+}
